556a59ca900598c5fed0a699b2946fd3fb18ea37
Omer Ahmed <omera407@gmail.com>
2022-08-19 21:08:50 -0600

    Player can now swing a staff by pressing the Enter key

--- MuscleWizard/Creatures/Player.gd
+++ MuscleWizard/Creatures/Player.gd
@@ -1,28 +1,48 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Movement variables
 export var moveSpeed = 400
 var velocity = Vector2.ZERO
 
+# Staff / weapon variables
+enum staffStates {IN_HAND, SWINGING}
+export var currentStaffState = staffStates.IN_HAND
+var staffOffset
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	staffOffset = $Sprite/Staff.position
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move_player()
+	handle_staff()
+
+func handle_staff():
+	match currentStaffState:
+		staffStates.IN_HAND:
+			if Input.is_action_pressed("ui_accept"):
+				currentStaffState = staffStates.SWINGING
+				$AnimationPlayer.play("StaffSwing")
+		#staffStates.SWINGING:
+			#$Staff.position.x += 5
+			#$Staff.rotation += 2
+			#pass
+
+# Called from within animations to say, let us know when the staff is done being swung
+func Set_Staff_State(newStaffState: int):
+	currentStaffState = newStaffState
 
 func move_player():
 	var newVelocity = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
 		newVelocity.x -= moveSpeed
-		$Sprite.flip_h = true
+		set_player_facing_direction(true)
 	if Input.is_action_pressed("move_right"):
-		$Sprite.flip_h = false
+		set_player_facing_direction(false)
 		newVelocity.x += moveSpeed
 	if Input.is_action_pressed("move_up"):
 		newVelocity.y -= moveSpeed
@@ -30,3 +50,11 @@ func move_player():
 		newVelocity.y += moveSpeed
 	velocity = newVelocity
 	move_and_slide(velocity)
+
+func set_player_facing_direction(facingLeft: bool):
+	#$Sprite.flip_h = facingLeft
+	if currentStaffState == staffStates.IN_HAND:
+		$Sprite.scale.x = -0.5 if facingLeft else 0.5
+		#scale.x = -1 if facingLeft else 1
+		#var newStaffX = staffOffset.x + (staffOffset.x * -2 * int(facingLeft))
+		#$Sprite/Staff.position.x = newStaffX
