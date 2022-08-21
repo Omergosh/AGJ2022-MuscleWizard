9ad03d7665bbfa60beb1e65c107a027026831895
Boris <bfleyshe@ualberta.ca>
2022-08-20 19:48:27 -0600

    Added simple dialogue and dialogue choice

--- MuscleWizard/UI/DialogueBox.gd
+++ MuscleWizard/UI/DialogueBox.gd
@@ -1,9 +1,32 @@
 extends Control
 
 
+var Text
+var Name
+var Portrait
+var filePath = "res://UI/Dialogue/dialogue.json"
+var txtDictionary
+var dialogueIndex = 0
+var currentTextScene = []
+export(bool) var debug = false
+var lastUsedDialogueKey
+var lastUsedDialogueIndex
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Popup.popup()
+	Text = $Popup/HBoxContainer/Text
+	Name = $Popup/HBoxContainer/MarginContainer/VBoxContainer/Name
+	Portrait = $Popup/HBoxContainer/MarginContainer/VBoxContainer/Portrait
+	var file = File.new()
+	file.open(filePath, File.READ)
+	txtDictionary = file.get_as_text()
+	file.close()
+	var err = JSON.parse(txtDictionary)
+	if err.result == null:
+		print(err)
+	txtDictionary = JSON.parse(txtDictionary).result
+	if debug:
+		showDialogue("testDialogue", 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,10 +34,28 @@ func _ready():
 #	pass
 
 
-# Dialogue data is a json struct with the following data:
+func playDialogue(dialogueKey):
+	showDialogue(dialogueKey, 0)
+
+# txtDictionary is a json struct with the following data:
 # text - What the speaker is saying. Try not to say too much or it will clip off.
 # name - The name to display for who is speaking
-# portrait - The sprite to use for the portrait
-func showDialogue(dialogueData):
-	$Label.text = dialogeData["text"]
+# portrait - The sprite to use for the portrait, as a str path
+func showDialogue(dialogueKey, index):
+	var dialogueData = txtDictionary[dialogueKey][index]
+	Text.text = dialogueData["text"]
+	Name.text = dialogueData["name"]
+	Portrait.texture = load(dialogueData["portrait"])
 	
+	lastUsedDialogueKey = dialogueKey
+	lastUsedDialogueIndex = index
+	$Popup.popup()
+
+
+func _on_Popup_popup_hide():
+	tryNextDialogue()
+
+func tryNextDialogue():
+	var dialogueData = txtDictionary[lastUsedDialogueKey]
+	if len(dialogueData)-1 > lastUsedDialogueIndex:
+		showDialogue(lastUsedDialogueKey, lastUsedDialogueIndex+1)
