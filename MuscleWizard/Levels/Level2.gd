fc5e67b3dda49f960eebb5f1579b74ff26dabfdb
I-Psychohistorian <92660661+I-Psychohistorian@users.noreply.github.com>
2022-08-21 01:33:33 -0700

    Atmosphere
    Added sounds to the final area for atmosphere. Accidentally broke shamblers in trying to add sound.

---
+++ MuscleWizard/Levels/Level2.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+var voice1 = false
+var voice2 = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Spook1_body_entered(body):
+	if voice1 == false:
+		$Voice1.play()
+		voice1 = true
+
+
+func _on_Spook2_body_entered(body):
+	if voice2 == false:
+		$Voice2.play()
+		voice2 = true
