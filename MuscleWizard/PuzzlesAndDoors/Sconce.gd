3983583efbe469a1a2773532680e78997a88aac5
I-Psychohistorian <92660661+I-Psychohistorian@users.noreply.github.com>
2022-08-20 20:17:09 -0700

    Reset some images

--- MuscleWizard/PuzzlesAndDoors/Sconce.gd
+++ MuscleWizard/PuzzlesAndDoors/Sconce.gd
@@ -15,7 +15,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	health = lerp(health, 2, 0.01)
-	print(health)
+	#print(health)
 	if health > 1:
 		lit = false
 	if lit == false:
