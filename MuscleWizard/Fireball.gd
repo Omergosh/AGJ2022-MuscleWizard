000e950a247cc9986279cdca3f62fe7b6e104be8
I-Psychohistorian <92660661+I-Psychohistorian@users.noreply.github.com>
2022-08-21 15:17:32 -0700

    AssetsForLater
    Added assets and fireball thingy for later

--- MuscleWizard/Fireball.gd
+++ MuscleWizard/Fireball.gd
@@ -8,8 +8,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$FireForm.play()
 
+
+#straightline travel from staff, explode on contact.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -17,4 +19,5 @@ func _ready():
 
 
 func _on_SummonTimer_timeout():
-	pass # Replace with function body.
+	$Fire.visible = true
+	$Fireform.visible = false
